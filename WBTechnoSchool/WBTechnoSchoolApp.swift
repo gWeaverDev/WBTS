189302133fd52c61efa03a31f5a2804dd31e6538
Yehor Tkachenko <tkachenko.egor3@wildberries.work>
2024-07-14 22:02:06 +0300

    created project

---
+++ WBTechnoSchool/WBTechnoSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBTechnoSchoolApp.swift
+//  WBTechnoSchool
+//
+//  Created by George Weaver on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBTechnoSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
